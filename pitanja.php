c8fb2e4ef8821e3cb352f3ddf3c377f821b18b44
Ivan Popic <ivan.popic99@gmail.com>
2020-12-30 14:00:44 +0100

    Nastavak web, iduce footer dodati

---
+++ pitanja.php
@@ -0,0 +1,25 @@
+<link href="style.css" rel="stylesheet">
+<link href="pitanja.css" rel="stylesheet" type="text/css">
+<?php
+    $name = $_POST['name'];
+    $visitor_email=$_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'EasyPeasy@gmail.com';
+    $email_subject="New Form Submission";
+
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Message: $message.\n";
+
+    $to = "EasyPeasy@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+
+?>
